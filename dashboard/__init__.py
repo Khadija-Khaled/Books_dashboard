7c12d9090028961e774ee783e6933a938a82b332
KhadijaHub <khadija.k4a@gmail.com>
2020-03-06 23:05:17 +0200

    2nd commit

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
 import pandas as pd
+import json
+import requests
+import xml.etree.ElementTree as ElementTree
+import re
 
 from dashboard import routes
 from dashboard.routes import app
--- dashboard/routes.py
+++ dashboard/routes.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 import pandas as pd
 import json
 import requests
+import xml.etree.ElementTree as ElementTree
+import re
 
 app = Flask(__name__)
 
@@ -14,25 +16,38 @@ def index():
 def dashboard():
     query = request.form.get("query")
     print("got query of",query)
+    #printed in the command window for tracking purposes
 
 
     key = "tElckxfXrsUO6Y6xHpBcA"
-    secret = "S47cOPu8AGUYha4LUUyUq0chuIbnrCkxgdnrwVanYg"
-    url = "https://www.goodreads.com/book/review_counts.json?"
+    #secret = "S47cOPu8AGUYha4LUUyUq0chuIbnrCkxgdnrwVanYg"
+    url = "https://www.goodreads.com/book/title.xml?"
     params = {
-        "isbns" : query,
-        "key" : key
+        "key" : key,
+        "title" : query
     }
 
-    res = requests.get(url, params=params)
-    res = json.loads(res.text)
-    print(res)
-    data = { 
-            "book_id": str(res['books'][0]['id']), 
-            "ratings_count": str(res['books'][0]['ratings_count']),
-            # "temp": str(res['main']['temp']) + ' C', 
-            # "pressure": str(res['main']['pressure']) + " hPa", 
-            # "humidity": str(res['main']['humidity']) + "%",
-            # "wind": str(res['wind']['speed']) + ' m/s'
-    } 
-    print(data)
+    result = requests.get(url, params=params)
+    res = ElementTree.fromstring(result.content)
+
+    filtered = re.compile('<.*?>')
+    book_description = re.sub(filtered, '', res[1][16].text)
+
+    rating_distribution = str(res[1][17][13].text).split("|")
+    rating_distribution = [i[2:] for i in rating_distribution]
+    rating_distribution.pop()
+
+    book_title = str(res[1][1].text)
+    book_author = str(res[1][26][0][1].text)
+    reviews_count = str(res[1][17][3].text)
+    average_rating = str(res[1][18].text)
+    num_pages = str(res[1][19].text)
+    #data = {"book_ISBN" : str(res[1][2].text)}
+
+    positive_counts = int(rating_distribution[0]) + int(rating_distribution[1]) + int(int(rating_distribution[0])/2)
+    negative_counts = int(rating_distribution[3]) + int(rating_distribution[4]) + int(int(rating_distribution[0])/2)
+
+    return render_template("dashboard.html", query=query, book_description = book_description,
+    reviews_count=reviews_count, rating_distribution = rating_distribution, book_title = book_title, 
+    book_author = book_author, average_rating = average_rating, num_pages = num_pages,
+    positive_counts=positive_counts, negative_counts = negative_counts)
